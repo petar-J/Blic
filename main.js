7878f4b7c43b17d56f9225785ec48dd43a66876a
petar-J <37311884+petar-J@users.noreply.github.com>
2018-10-06 17:52:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
 
-            $(document).ready(function(){
-                $('a').click(function(event){
-                    var target = $(this.hash).offset();
-                    event.preventDefault();
-                    window.scrollTo({top: target.top-60, 
-                                left: 0, 
-                                behavior: 'smooth' 
-                    });
-                });
+$(document).ready(function(){
+    $('a').click(function(event){
+        var target = $(this.hash).offset();
+        event.preventDefault();
+        window.scrollTo({top: target.top-60, 
+                    left: 0, 
+                    behavior: 'smooth' 
+        });
+    });
 
-                $(window).scroll(function(){
-                    let cond = $(document).scrollTop() > $('.container').offset().top;
-                    if (cond){
-                        $('.active').text('Top');
-                        $('.active').click(function(event){
-                            if (cond){
-                                event.preventDefault();
-                                window.scrollTo({top: 0, 
-                                            left: 0, 
-                                            behavior: 'smooth' 
-                                            });
-                            };
-                        });
-                    }else{
-                        $('.active').text('Naslovna');
-                    }
-                });
+    $(window).scroll(function(){
+        
+        let nav_items = document.querySelectorAll('.nav_container>*');
+        for (let i = 0; i<nav_items.length; i++){
+            nav_items[i].classList.remove('active');
 
-                $('.drop-btn').click(function(){
-                    $('.drop').slideToggle(300)
-                });
-                
-                $('.drop li').click(function(){
-                    $('.drop').slideUp(300)
-                });
-            });
+            let item = document.querySelector(nav_items[i].hash);
+            if (item){
+                if (item.getBoundingClientRect().top-100 <= 0 && item.getBoundingClientRect().bottom-100 > 0){
+                    nav_items[i].classList.add('active');
+                }
+            };
+        };
+
+    });
+
+    $('.drop-btn').click(function(){
+        $('.drop').slideToggle(300)
+    });
+    
+    $('.drop li').click(function(){
+        $('.drop').slideUp(300)
+    });
+});
